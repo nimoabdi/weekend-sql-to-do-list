7ce251522630684a81bdd8740bf526be2f8fbeaf
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-28 16:30:33 -0500

    jquery

--- server/public/routes/todo.router.js
+++ server/public/routes/todo.router.js
@@ -3,4 +3,19 @@ const pool = require('../modules/pool')
 const router = express.Router();
 // database connection
 
+// GET
+
+
+
+
+//POST
+
+
+
+// PUT
+
+
+
+// DELETE
+
 module.exports = router;
